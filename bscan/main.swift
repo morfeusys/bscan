0bf539baeda6d5f4804c796c8a816787dd409167
Dmitry Chechyotkin <che@almworks.com>
2014-08-20 14:41:08 +0400

    Initial Commit

---
+++ bscan/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  bscan
+//
+//  Created by Dmitry Chechyotkin on 20/08/14.
+//  Copyright (c) 2014 morfeusys. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
